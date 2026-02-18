93bdac630d626be37f3fe45ede0a3ccc208174a0
prajinpokhrel <prajinpokhrel@gmail.com>
2026-02-18 21:50:31 +0545

    Modified dashboard than before

--- app/src/main/java/com/example/myproject/DashboardActivity2.kt
+++ app/src/main/java/com/example/myproject/DashboardActivity2.kt
@@ -1,13 +1,20 @@
-package com.example.myproject
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myproject.ui.theme.Pink40
+import com.example.myproject.ui.theme.Purple80
+
 
 
 class DashboardActivity2 : ComponentActivity() {
@@ -22,9 +29,34 @@ class DashboardActivity2 : ComponentActivity() {
 
 
 @Composable
-fun DashboardActivity(){
+fun DashboardActivity() {
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(Purple80),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
 
+            Text(
+                text = "Dashboard",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Pink40
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "Welcome to MarketO 🎉",
+                fontSize = 18.sp
+            )
+        }
+    }
 }
+
 
 @Preview
 @Composable
